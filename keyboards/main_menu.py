4b28e376cfeba3c47ae39ad8fe3d5dec51f4018c
NikZyaba <nikitazjabko@rambler.ru>
2026-02-25 20:11:32 +0300

    Создал клавиатуру главного меню

---
+++ keyboards/main_menu.py
@@ -0,0 +1,14 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def get_main_menu():
+    """Основное меню бота"""
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Все места", callback_data="all_seats"),
+            InlineKeyboardButton(text="Мои брони", callback_data="my_orders"),
+            InlineKeyboardButton(text="Забронировать новое место", callback_data="create_order")
+        ],
+        [InlineKeyboardButton(text="ℹ️ Помощь", callback_data="help")]
+    ])
+    return keyboard
