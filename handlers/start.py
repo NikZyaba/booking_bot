a4821e3adfe4842e8b4a6f18b447adf3e7ce4f02
NikZyaba <nikitazjabko@rambler.ru>
2026-02-25 20:12:09 +0300

    Добавил команду help

--- handlers/start.py
+++ handlers/start.py
@@ -1,6 +1,9 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 
+# Импорты кнопок
+from keyboards.main_menu import get_main_menu
+
 
 router = Router()
 
@@ -18,4 +21,19 @@ async def cmd_start(message:types.Message):
         "/make_order - забронировать новое место\n"
     )
     # Потом будет клавиатура для меню----------------------↓
-    await message.answer(text=welcome_text, reply_markup=None)
+    await message.answer(text=welcome_text, reply_markup=get_main_menu())
+
+
+@router.message(Command("help"))
+async def cmd_help(message:types.Message):
+    help_text = (
+        f"Эта команда описывает основные команды бота\n"
+        "⚡ Доступные команды:\n"
+        "/all_seats - запросить все места ресторана\n"
+        "/my_orders - мои заказы\n"
+        "/make_order - забронировать новое место\n"
+        "/seat_by_date - показать места по дате\n"
+        "Пока многие функции находятся в разработке"
+    )
+    # Потом будет клавиатура для меню-------------------↓
+    await message.answer(text=help_text, reply_markup=get_main_menu())
