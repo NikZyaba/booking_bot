c5d27d1a00f9bdc58edf0313f5529eb54cb06b1e
NikZyaba <nikitazjabko@rambler.ru>
2026-03-07 10:02:51 +0300

    Обновил клавиатуру согласно ТЗ

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -5,10 +5,16 @@ def get_main_menu():
     """Основное меню бота"""
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [
-            InlineKeyboardButton(text="Показать все места", callback_data="cmd_show_all_seats"),
-            InlineKeyboardButton(text="Мои брони", callback_data="my_orders"),
-            InlineKeyboardButton(text="Забронировать новое место", callback_data="create_order")
+            InlineKeyboardButton(text="📍 Все места", callback_data="cmd_show_all_seats"),
         ],
-        [InlineKeyboardButton(text="ℹ️ Помощь", callback_data="help")]
+        [
+            InlineKeyboardButton(text="📅 Забронировать новое место", callback_data="create_order"),
+        ],
+        [
+            InlineKeyboardButton(text="📋 Мои брони", callback_data="my_orders"),
+        ],
+        [
+            InlineKeyboardButton(text="ℹ️ Помощь", callback_data="help")
+        ]
     ])
     return keyboard
